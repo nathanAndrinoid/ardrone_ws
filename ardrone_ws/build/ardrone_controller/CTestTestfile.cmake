a2852a07c435a7aa71802d323b2bf3056b9adc7b
nathanAndrinoid <nathana@nmsu.edu>
2015-02-17 00:08:47 -0700

    fix folders

--- ardrone_ws/build/ardrone_controller/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nanderso/ardrone_ws/src/ardrone_controller
-# Build directory: /home/nanderso/ardrone_ws/build/ardrone_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
